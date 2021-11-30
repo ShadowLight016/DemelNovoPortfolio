7475b19cd16905b2a5187767d23e551edf4a0fb2
Demel Novo <demel.novo.2@gmail.com>
2021-12-01 04:48:07 +0800

    Changed Some Codes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,28 +5,41 @@ const Skills = () => {
         <div className="container m-5 vh-100">
             <h2 className="text-prim">Programming Languages And Frameworks That I Have Used</h2>
             <ul id="prog-lang">
-                <li><i className="devicon-arduino-plain" /> Arduino</li>
-                <li><i className="devicon-bootstrap-plain" /> Bootstrap</li>
-                <li><i className="devicon-csharp-plain" /> C#</li>
-                <li><i className="devicon-cplusplus-plain" /> C++</li>
-                <li><i className="devicon-css3-plain" /> CSS</li>
-                <li><i className="devicon-html5-plain" /> HTML</li>
-                <li><i className="devicon-java-plain" /> Java</li>
-                <li><i className="devicon-javascript-plain" /> JavaScript</li>
-                <li><i className="devicon-laravel-plain" /> Laravel</li>
-                <li><i className="devicon-nextjs-original" /> Next.js</li>
-                <li><i className="devicon-php-plain" /> PHP</li>
-                <li><i className="devicon-react-original" /> React</li>
-                <li><i className="devicon-sass-original" /> SASS</li>
-                <li><i className="devicon-tailwindcss-plain" /> Tailwind</li>
-                <li>
-                    <i className="text-center">
-                        <svg width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12.7458 24.6164C19.2822 24.6164 24.5811 19.3175 24.5811 12.7811C24.5811 6.24464 19.2822 0.945801 12.7458 0.945801C6.20936 0.945801 0.910522 6.24464 0.910522 12.7811C0.910522 19.3175 6.20936 24.6164 12.7458 24.6164Z" fill="#212529"/>
-                            <path d="M13.2229 8.12103L9.78696 17.4413H8.57015L5.1897 8.12103H6.40651L8.99548 15.5181C9.08476 15.7863 9.14793 16.0625 9.1841 16.3429H9.20999C9.24875 16.0552 9.31819 15.7725 9.41711 15.4996L12.0505 8.10254L13.2229 8.12103ZM14.5655 17.4413V8.12103H17.2173C17.906 8.08418 18.5852 8.29399 19.1331 8.7128C19.3623 8.89782 19.5456 9.13327 19.6688 9.4008C19.7919 9.66832 19.8517 9.96068 19.8433 10.2551C19.8505 10.7494 19.6959 11.2326 19.4031 11.6309C19.1076 12.0276 18.6929 12.3193 18.2196 12.4631V12.489C18.792 12.5351 19.3282 12.7875 19.7286 13.1991C20.1157 13.6258 20.319 14.1879 20.2945 14.7636C20.3066 15.1304 20.2357 15.4952 20.0871 15.8307C19.9386 16.1663 19.7161 16.464 19.4364 16.7016C18.8311 17.2066 18.0605 17.4701 17.2728 17.4413H14.5655ZM15.675 9.10484V12.1154H16.7846C17.2898 12.1392 17.7868 11.9818 18.1863 11.6716C18.3594 11.5208 18.4957 11.3325 18.5848 11.1209C18.6739 10.9094 18.7134 10.6803 18.7004 10.4511C18.7004 9.5462 18.1049 9.09374 16.914 9.09374L15.675 9.10484ZM15.675 13.0955V16.4501H17.1544C17.6862 16.4809 18.2113 16.3195 18.6338 15.9952C18.8101 15.8407 18.9493 15.6483 19.0408 15.4325C19.1324 15.2167 19.1741 14.983 19.1627 14.7488C19.1627 13.6491 18.4132 13.0992 16.914 13.0992L15.675 13.0955Z" fill="white"/>
-                        </svg>
-                    </i> VB.Net
-                </li>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <li><i className="devicon-arduino-plain" /> Arduino</li>
+                            <li><i className="devicon-bootstrap-plain" /> Bootstrap</li>
+                            <li><i className="devicon-csharp-plain" /> C#</li>
+                            <li><i className="devicon-cplusplus-plain" /> C++</li>
+                            <li><i className="devicon-css3-plain" /> CSS</li>
+                        </div>
+                        <div className="col">
+                            <li><i className="devicon-html5-plain" /> HTML</li>
+                            <li><i className="devicon-java-plain" /> Java</li>
+                            <li><i className="devicon-javascript-plain" /> JavaScript</li>
+                            <li><i className="devicon-laravel-plain" /> Laravel</li>
+                            <li><i className="devicon-nextjs-original" /> Next.js</li>
+                        </div>
+                        <div className="col">
+                            <li><i className="devicon-php-plain" /> PHP</li>
+                            <li><i className="devicon-react-original" /> React</li>
+                            <li><i className="devicon-sass-original" /> SASS</li>
+                            <li><i className="devicon-tailwindcss-plain" /> Tailwind</li>
+                            <li>
+                                <i className="text-center">
+                                    <svg width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12.7458 24.6164C19.2822 24.6164 24.5811 19.3175 24.5811 12.7811C24.5811 6.24464 19.2822 0.945801 12.7458 0.945801C6.20936 0.945801 0.910522 6.24464 0.910522 12.7811C0.910522 19.3175 6.20936 24.6164 12.7458 24.6164Z" fill="#212529"/>
+                                        <path d="M13.2229 8.12103L9.78696 17.4413H8.57015L5.1897 8.12103H6.40651L8.99548 15.5181C9.08476 15.7863 9.14793 16.0625 9.1841 16.3429H9.20999C9.24875 16.0552 9.31819 15.7725 9.41711 15.4996L12.0505 8.10254L13.2229 8.12103ZM14.5655 17.4413V8.12103H17.2173C17.906 8.08418 18.5852 8.29399 19.1331 8.7128C19.3623 8.89782 19.5456 9.13327 19.6688 9.4008C19.7919 9.66832 19.8517 9.96068 19.8433 10.2551C19.8505 10.7494 19.6959 11.2326 19.4031 11.6309C19.1076 12.0276 18.6929 12.3193 18.2196 12.4631V12.489C18.792 12.5351 19.3282 12.7875 19.7286 13.1991C20.1157 13.6258 20.319 14.1879 20.2945 14.7636C20.3066 15.1304 20.2357 15.4952 20.0871 15.8307C19.9386 16.1663 19.7161 16.464 19.4364 16.7016C18.8311 17.2066 18.0605 17.4701 17.2728 17.4413H14.5655ZM15.675 9.10484V12.1154H16.7846C17.2898 12.1392 17.7868 11.9818 18.1863 11.6716C18.3594 11.5208 18.4957 11.3325 18.5848 11.1209C18.6739 10.9094 18.7134 10.6803 18.7004 10.4511C18.7004 9.5462 18.1049 9.09374 16.914 9.09374L15.675 9.10484ZM15.675 13.0955V16.4501H17.1544C17.6862 16.4809 18.2113 16.3195 18.6338 15.9952C18.8101 15.8407 18.9493 15.6483 19.0408 15.4325C19.1324 15.2167 19.1741 14.983 19.1627 14.7488C19.1627 13.6491 18.4132 13.0992 16.914 13.0992L15.675 13.0955Z" fill="white"/>
+                                    </svg>
+                                </i> VB.Net
+                            </li>
+                        </div>
+                    </div>
+                </div>
+                
+               
+                
             </ul>
         </div>
     )
